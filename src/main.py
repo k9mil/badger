901911c2b879b6b79b1e9a58960bb8bd8b0182ed
Kamil Zak <kamilzak00@gmail.com>
2022-05-12 14:46:42 +0100

    feat: added logging to meet requirements

--- src/main.py
+++ src/main.py
@@ -1,18 +1,51 @@
-import csv
+import csv, logging
+
+from datetime import datetime as dt
 
 class Person:
     def __init__(self, id, first_name, last_name, street, zip, city, type, last_check_in, job, phone, company):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
-        self.street = street
-        self.zip = zip
-        self.city = city
+        self.street = self._is_valid_street(street)
+        self.zip = self._is_valid_zip(zip)
+        self.city = self._is_valid_city(city)
         self.type = type
-        self.last_check_in = last_check_in
+        self.last_check_in = self._is_valid_last_check_in(last_check_in)
         self.job = job
         self.phone = phone
-        self.company = company
+        self.company = self._is_valid_company(company)
+
+    def _is_valid_street(self, street):
+        if not street:
+            logging.warning(f"Row number {self.id} is missing a required field: Street")
+        return street
+
+    def _is_valid_zip(self, zip):
+        if not zip:
+            logging.warning(f"Row number {self.id} is missing a required field: Zip")
+        return zip
+
+    def _is_valid_city(self, city):
+        if not city:
+            logging.warning(f"Row number {self.id} is missing a required field: City")
+        return city
+
+    def _is_valid_last_check_in(self, last_check_in):
+        if not last_check_in:
+            logging.warning(f"Row number {self.id} is missing a required field: Last Check-In Date")
+            return None
+        return dt.strptime(last_check_in, "%d/%m/%Y")
+
+    def _is_valid_company(self, company):
+        if not company:
+            logging.warning(f"Row number {self.id} is missing a required field: Company")
+        return company
+
+    def _has_fields_data(self):
+        if len(self.first_name) + len(self.last_name) + len(self.street) + len(self.zip) + len(self.city) + \
+            len(self.type)+ len(str(self.last_check_in)) + len(self.job) + len(self.phone) + len(self.company) < 1:
+                logging.warning(f"Row number {self.id} does not contain any data!")
 
 def read_file(list_of_people):
     with open("data.csv", mode = "r", encoding = "utf8") as csv_file:
